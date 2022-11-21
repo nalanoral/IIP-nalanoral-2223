dcaaf4c371609c8df42a096a6a02cadec4c2fe33
N@lan <nalan.oral@student.odisee.be>
2022-11-21 09:20:55 +0100

    Les01

---
+++ SlnLes01VisualStudioEnWpf/ConsoleHalloWereld/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleHalloWereld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Wereld!");
+            Console.Write("Hoe heet je? ");
+            string name = Console.ReadLine();
+            Console.Write("Aangename kennismaking, ");
+            Console.WriteLine(name);
+            Console.WriteLine("druk een toestom verder te gaan...");
+            Console.ReadKey();
+        }
+    }
+}
