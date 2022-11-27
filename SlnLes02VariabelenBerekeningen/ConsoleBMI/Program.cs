1747e77d49f0a982d3e9a40e9ad4773f4ec02117
N@lan <nalan.oral@student.odisee.be>
2022-11-27 15:38:27 +0100

    Les 02. VARIABELEN EN BEREKENINGEN

---
+++ SlnLes02VariabelenBerekeningen/ConsoleBMI/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleBMI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Head
+            Console.WriteLine("BMI CALCULATOR");
+            Console.WriteLine("==============");
+
+            //lengte
+            Console.WriteLine("Lengte (in cm): ");
+
+
+            // string omzetten in een double
+            double lengte;
+            lengte = Convert.ToInt32(Console.ReadLine());
+
+
+            // gewicht opvragen
+            Console.Write("Gewicht (in kg): ");
+
+            double gewicht;
+            gewicht = Convert.ToInt32(Console.ReadLine());
+
+
+            // bmi berekenen en afronden op 1 cijfer na de komma
+            double BMI = gewicht / (lengte / 100 * lengte / 100);
+
+
+            Console.WriteLine("Je BMI bedraagt " + Math.Round(BMI, 1));
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ SlnLes02VariabelenBerekeningen/WpfBMI/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfBMI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnBerekenen_Click(object sender, RoutedEventArgs e)
+        {
+            double getalcm = Convert.ToDouble(txtbxCM.Text);
+            double getalkg = Convert.ToDouble(txtbxKg.Text);
+            double BMI = getalkg / (getalcm / 100 * getalcm / 100);
+            LblBMI.Content = "BMI :" + Math.Round(BMI, 1);
+        }
+    }
+}
