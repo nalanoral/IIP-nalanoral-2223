49785b8f976c26a9273bfc100ebf88c817ba4fa6
N@lan <nalan.oral@student.odisee.be>
2022-12-04 19:00:04 +0100

    03.A SELECTIES

---
+++ SlnLes03aSelecties/ConsoleRaden/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleRaden
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Het getal dat moet geraad worden
+            int getal = 3;
+            Console.Write("Raad een getal tussen 1 en 10:");
+
+            // De gok van de user
+            int raad = Convert.ToInt32(Console.ReadLine());
+
+            // Als de gok groter is dan het getal
+            if (raad > getal) 
+            { 
+                Console.WriteLine("De gok was te hoog"); 
+            }
+
+            // Als de gok kleiner is dan het getal
+           else if (raad < getal)
+            {
+                Console.WriteLine("De gok was te laag");
+            }
+
+           // Anders van de twee vorige(dus als de gok gelijk is aan het getal)
+            else
+            {
+                Console.WriteLine("Je hebt het juiste getal geraden !");
+            }
+            Console.ReadKey();  
+        }
+    }
+}
---
+++ SlnLes03aSelecties/WpfSliderKleur/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfSliderKleur
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SldKleur_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            // De value van de slider aantonen 
+            txtblkWaarde.Text = Convert.ToString(SldKleur.Value);
+
+            // Als de value kleiner en gelijk is aan 25 dan is de tekstkleur groen
+            if (Math.Round(SldKleur.Value) <= 25)
+            {
+                // Aangeven van een kleur aan txtblkWaarde
+                txtblkWaarde.Foreground = Brushes.Green;
+            }
+
+            // Als de value groter dan 25 of kleiner en gelijk  aan 50 dan is de tekstkleur geel
+            else if ((Math.Round(SldKleur.Value) > 25) && (SldKleur.Value <= 50))
+            {
+                txtblkWaarde.Foreground = Brushes.Yellow;
+            }
+
+            // Als de value groter  dan 50 of kleiner en gelijk is aan 75 dan is de tekstkleur oranje
+            else if ((Math.Round(SldKleur.Value) > 50) && (SldKleur.Value <= 75))
+            {
+                txtblkWaarde.Foreground = Brushes.Orange;
+            }
+            else
+            {
+                txtblkWaarde.Foreground = Brushes.Red;
+            }
+        }
+    }
+}
