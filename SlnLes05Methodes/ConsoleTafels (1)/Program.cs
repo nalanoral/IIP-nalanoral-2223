db7389513b5b3f3f6e47bd2f741a5822c9d76138
N@lan <nalan.oral@student.odisee.be>
2022-12-18 20:03:11 +0100

    project wpf

---
+++ SlnLes05Methodes/ConsoleTafels (1)/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTafels__1_
+{
+    internal class Program
+    {
+        private static string DrukTafel(int getal, int lengte)
+        {
+            // maaltafel genereren
+            string tafel = "";
+            for (int i = 1; i <= lengte; i++)
+            {
+                int vermenigvuldiging = getal * i;
+                tafel += $"{getal} x {i} = {vermenigvuldiging}";
+                tafel += Environment.NewLine;
+            }
+            return tafel;
+        }
+        static void Main(string[] args)
+        {
+            // getal opvragen
+            Console.Write("Geef een getal: ");
+            int getal = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Geef een getal: ");
+            int lengte = Convert.ToInt32(Console.ReadLine());
+            string maaltafel = DrukTafel(getal, lengte);
+
+            Console.WriteLine(value: $" {maaltafel}");
+
+            Console.ReadKey();
+        }
+    }
+    
+}
---
+++ SlnLes05Methodes/ConsoleTafels (2)/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTafels__2_
+{
+    internal class Program
+    {
+        private static int VraagPositiefGetal()
+        {
+            // getal opvragen
+            Console.Write("Geef een getal: ");
+            int getal = Convert.ToInt32(Console.ReadLine());
+            if (getal < 0)
+            {
+                Console.WriteLine("Het getal moet positief zijn. Geef een getal ");
+            }
+            else
+            {
+                Console.Write("");
+            }
+            return getal;
+        }
+        private static string DrukTafel(int getal, int lengte)
+        {
+            // maaltafel genereren
+            string tafel = "";
+            for (int i = 1; i <= lengte; i++)
+            {
+                int vermenigvuldiging = getal * i;
+                tafel += $"{getal} x {i} = {vermenigvuldiging}";
+                tafel += Environment.NewLine;
+            }
+            // returning naar de main
+            return tafel;
+        }
+
+        static void Main(string[] args)
+        {
+
+            int basisGetal = VraagPositiefGetal();
+            int lengte = VraagPositiefGetal();
+
+            // maaltafel laten genereren
+            string tafel = DrukTafel(basisGetal, lengte);
+            // tafel weergeven in de console
+            Console.WriteLine(tafel);
+            Console.ReadLine();
+        }
+    }
+}
